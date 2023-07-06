40ea1896f90d53ca63f5b39a45dc1c3bad7500df
k23023kk <136865700+k23023-university-student@users.noreply.github.com>
2023-07-06 10:52:49 +0900

    12回:issue1関数名を修正

--- prg1K/prg1/12/12_issue1_k23023.c
+++ prg1K/prg1/12/12_issue1_k23023.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int sumOf(int num){
+int factOf(int num){
     int kaizyou = 1;
     for(int i=num;i>=1;i--){
        kaizyou = kaizyou * i;
@@ -10,6 +10,6 @@ int main(int argc, const char * argv[]){
     int n;
     printf("n? ");
     scanf("%d",&n);
-    printf("%dの階乗は %d\n",n,sumOf(n));
+    printf("%dの階乗は %d\n",n,factOf(n));
     return 0;
 }
