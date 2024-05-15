da061bad08a48c6393c9a2900a40a468cd1ccb69
k23023kk <136865700+k23023-university-student@users.noreply.github.com>
2024-05-15 13:55:34 +0900

    Java演習 :テストコードを修正

--- java-kougi/04/src/Fibonacci.java
+++ java-kougi/04/src/Fibonacci.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class Fibonacci {
     public IntCollector get(int n){
 
         IntCollector clct = new IntCollector();
+        if(n < 0) return clct;
         clct.add(0);
+        if(n == 0) return clct;
         clct.add(1);
+        if(n == 1) return clct;
         int count = 2;//すでに2つ挿入したのでカウンタを2に
         while(true){
             int n_2=0,n_1 = 0;
@@ -18,7 +23,17 @@ public class Fibonacci {
     }
     public static void main(String[] args){
         Fibonacci fib = new Fibonacci();
+        System.out.print("get(10000): ");
         fib.get(10000).print();// [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+        System.out.print("get(200): ");
         fib.get(200).print();// [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+        System.out.print("get(-1): ");
+        fib.get(-1).print();
+        System.out.print("get(0): ");
+        System.out.println(Arrays.toString(fib.get(0).getArray()));
+        System.out.print("get(1): ");
+        System.out.println(Arrays.toString(fib.get(1).getArray()));
+        System.out.print("get(2): ");
+        System.out.println(Arrays.toString(fib.get(2).getArray()));
     }
 }
