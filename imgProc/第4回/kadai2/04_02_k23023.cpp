7a791f05cfd339c89f3432184da464c499090c78
k23023kk <136865700+k23023-university-student@users.noreply.github.com>
2024-05-21 23:34:09 +0900

    画像処理演習: 第4回課題2コード部分の修正

--- imgProc/第4回/kadai2/04_02_k23023.cpp
+++ imgProc/第4回/kadai2/04_02_k23023.cpp
@@ -3,19 +3,22 @@
 #include <opencv2/opencv.hpp>
 //画像ファイル (サイズは小さめが良い)
 #define FILE_NAME "./image2.jpg"
-#define WINDOW_NAME_INPUT "input"
-#define WINDOW_NAME_OUTPUT "output"
 #define COLOR_MAX (256)
 int main(int argc, const char * argv[]) {
 
  int i, x, y;//アクセス用の変数
- 
+ int hist[COLOR_MAX];//ヒストグラム用配列
+
+ for(i=0;i<COLOR_MAX;i++){//ヒストグラム用の配列の初期化
+
+    hist[i] = 0;
+}
+
  //画像の入力
  cv::Mat src_img; //画像の型と変数
 
  src_img = cv::imread(FILE_NAME, 0); //画像の読み込みグレースケール
-
-
+ 
 
 
  if (src_img.empty()) { //入力失敗の場合
@@ -23,24 +26,21 @@ int main(int argc, const char * argv[]) {
  return (-1);
  }
 
-
-
-//閾値を超えたピクセル数を計算
-
-int pixelCount = 0;//ピクセル数のカウント
+//ヒストグラムの生成
 for(int y=0;y<src_img.rows;y++){
     for(x=0;x<src_img.cols;x++){
 
         //画素値の取得
         unsigned char s = src_img.at<unsigned char>(y,x);
-        if(s < 121){ //閾値が121より小さいとき
-            
-            pixelCount++;
-        }
+        hist[(int)s]++;//画素数の計算
 
     }
 
 }
-fprintf(stdout,"%d\n",pixelCount);
+//出力
+for(i=0;i<COLOR_MAX;i++){
+
+    fprintf(stdout,"%d %d\n",i, hist[i]);
+}
  return 0;
 }
