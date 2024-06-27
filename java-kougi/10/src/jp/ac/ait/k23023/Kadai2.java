161ffb3d2839a53e35217017539f25951da1ed13
k23023kk <136865700+k23023-university-student@users.noreply.github.com>
2024-06-27 12:18:00 +0900

    Java演習: 第10回分の課題2のテストコードの修正

--- java-kougi/10/src/jp/ac/ait/k23023/Kadai2.java
+++ java-kougi/10/src/jp/ac/ait/k23023/Kadai2.java
@@ -14,80 +14,41 @@ public class Kadai2 {
             System.out.printf("パスワードのルール基準を満たしていません。%s \n", e.getMessage());
         }
 
-        List<String> passwords = Arrays.asList(
-                "a",
-                "+dR-fb5MT97xA0cJ2RYq!eRam8L12zXp/O7+mあ+Ik0AE3.DIJiE+E2",
-                "FGHVFGJBGTYJGYUJGJHGYUJHGFTYUJHGYUJNHGYUJH",
-                "dodhdfohfoehfoeihfoewhfioewfhewoifhewiofhoiew",
-                "58326547236532653275693256236598325628356326858923",
-                "********************************@@@@@@@@@@@",
-                "fefhjewhfoewfheoheowhewo6487325837258732@@@",
-                "FJEHFOEWHFOEWHFGOEWHGOEWHGOEWH5732563275***@@",
-                "wqheowqhiqwhoiwqhriowqHDjoiq@@@",
-                "fnakneoafebiabeiFYUFUFUFU343264823764238768237",
-                "1@48k.TBCpassword",
-                "1@48k.TBCpaSSwoRd",
-                "xj.ArG*GKzT@bkq?1!p@ssw0rDll+7q/ rIBgB+s",
-                "jsTUGUwagPp0r?vg2yMS2q+fFX",
-                "e9-Mbhhtz9CqwertyH3/FekMCz=dwzo5FC",
-                "QbZ.isbA8!asdFghKU=k#a+Jd3+n/K5XqXf5sFx",
-                ".Cm@nKHtTW1*3bL8e4Sbgs0ooE4+e!Q@=BZdqcfElWsEGcGFm!j-VYAjUiT*X@Y6PGko?.@d_tT5@dNtQ/Q!h*bAKhR..#xqS",
-                "j#5zA+@*JNh+-7ZXCVBNvfqi_jOBVKmMw?fL*-dbXVk",
-                "/8.zeKCg*.myaAbcDefabcdef1Bp0UVwWSuJdp0Gw6lGZZe+3JrlD7@OhdQ9We!",
-                "osPEirT_JEq.!GANHmIi5123456_pJ=bf_CY++a=ScLNZsMz1KVikse2*34nwEVtVgW_xj?nr"
+
+
+        List<String> passwordsForTest = Arrays.asList(
+                ".Cm@nKHtTW1*3bL8e4Sbgs0ooE4+e!Q@=BZdqcfElWsEGcGFm!j-VYAjUiT*X@Y6PGko?.@d_tT5@dNtQ/Q!h*bAKhR..#xqSdfwqfhwoqhfowqhfowqhowqifhiowqhs", // パスワードのルール基準を満たしていません。パスワードの長さが不正です
+                ".Cm@nKHtTW1*3bL8e4Sbgs0ooE4+e!Q@=BZdqcfElWsEGcGFm!j-VYAjUiT*X@Y6PGko?.@d_tT5@dNtQ/Q!h*bAKhR..#xqSdfwqfhwoqhfowqhfowqhowqifhiowqh", // ok
+                "a", // パスワードのルール基準を満たしていません。パスワードの長さが不正です
+                ".Cm@nKHtTW1*3bL", // パスワードのルール基準を満たしていません。パスワードの長さが不正です
+                ".Cm@nKHtTW1*3bLe", // ok
+                ".Cm@nKHtTW1*3bL8e4Sbgs0ooE4+e!Q@=BZdqcfElWsEGcGFm!j-VYAjUiT*X@Y6PGko?.@d_tT5@dNtQ/Q!h*bAKhR..#xqS", // ok
+                "+dR-fb5MT97xA0cJ2RYq!eRam8L12zXp/O7+mあ+Ik0AE3.DIJiE+E2", // パスワードのルール基準を満たしていません。使用不可能な文字を検知しました:あ
+                "FGHVFGJBGTYJGYUJGJHGYUJHGFTYUJHGYUJNHGYUJH", // パスワードのルール基準を満たしていません。小文字アルファベット、大文字アルファベット、数字、使用可能な記号を少なくとも一つずつ含む必要があります
+                "dodhdfohfoehfoeihfoewhfioewfhewoifhewiofhoiew", // パスワードのルール基準を満たしていません。小文字アルファベット、大文字アルファベット、数字、使用可能な記号を少なくとも一つずつ含む必要があります
+                "58326547236532653275693256236598325628356326858923", // パスワードのルール基準を満たしていません。小文字アルファベット、大文字アルファベット、数字、使用可能な記号を少なくとも一つずつ含む必要があります
+                "********************************@@@@@@@@@@@", // パスワードのルール基準を満たしていません。小文字アルファベット、大文字アルファベット、数字、使用可能な記号を少なくとも一つずつ含む必要があります
+                "fefhjewhfoewfheoheowhewo6487325837258732@@@", // パスワードのルール基準を満たしていません。小文字アルファベット、大文字アルファベット、数字、使用可能な記号を少なくとも一つずつ含む必要があります
+                "FJEHFOEWHFOEWHFGOEWHGOEWHGOEWH5732563275***@@", // パスワードのルール基準を満たしていません。小文字アルファベット、大文字アルファベット、数字、使用可能な記号を少なくとも一つずつ含む必要があります
+                "wqheowqhiqwhoiwqhriowqHDjoiq@@@", // パスワードのルール基準を満たしていません。小文字アルファベット、大文字アルファベット、数字、使用可能な記号を少なくとも一つずつ含む必要があります
+                "fnakneoafebiabeiFYUFUFUFU343264823764238768237", // パスワードのルール基準を満たしていません。小文字アルファベット、大文字アルファベット、数字、使用可能な記号を少なくとも一つずつ含む必要があります
+                "1@48k.TBCpassword", // パスワードのルール基準を満たしていません。パスワード中に使用不可能な文字列を検出しました
+                "1@48k.TBCpaSSwoRd", // パスワードのルール基準を満たしていません。パスワード中に使用不可能な文字列を検出しました
+                "xj.ArG*GKzT@bkq?1!p@ssw0rDll+7q/ rIBgB+s", // パスワードのルール基準を満たしていません。パスワード中に使用不可能な文字列を検出しました
+                "e9-Mbhhtz9CqwertyH3/FekMCz=dwzo5FC", // パスワードのルール基準を満たしていません。パスワード中に使用不可能な文字列を検出しました
+                "QbZ.isbA8!asdFghKU=k#a+Jd3+n/K5XqXf5sFx", // パスワードのルール基準を満たしていません。パスワード中に使用不可能な文字列を検出しました
+                "j#5zA+@*JNh+-7ZXCVBNvfqi_jOBVKmMw?fL*-dbXVk", // パスワードのルール基準を満たしていません。パスワード中に使用不可能な文字列を検出しました
+                "/8.zeKCg*.myaAbcDefabcdef1Bp0UVwWSuJdp0Gw6lGZZe+3JrlD7@OhdQ9We!", // パスワードのルール基準を満たしていません。パスワード中に使用不可能な文字列を検出しました
+                "osPEirT_JEq.!GANHmIi5123456_pJ=bf_CY++a=ScLNZsMz1KVikse2*34nwEVtVgW_xj?nr" // パスワードのルール基準を満たしていません。パスワード中に使用不可能な文字列を検出しました
         );
-
-        passwords.stream().forEach(o ->{
-            System.out.printf("// %s -> ",o);
-            try {
-                PasswordManager.validate(o);
-            } catch (PasswordRulesViolationException e) {
-                System.out.printf("パスワードのルール基準を満たしていません。%s \n", e.getMessage());
-            }
-        });
-
-
-
-        // --- Comment: 16文字以下のパスワードはアウト
-        // a
-        // --- Comment: 使用不可の文字が含まれるとアウト
-        // +dR-fb5MT97xA0cJ2RYq!eRam8L12zXp/O7+mあ+Ik0AE3.DIJiE+E2
-        // --- Comment: 大文字のみであるとアウト
-        // FGHVFGJBGTYJGYUJGJHGYUJHGFTYUJHGYUJNHGYUJH
-        // --- Comment: 小文字のみであるとアウト
-        // dodhdfohfoehfoeihfoewhfioewfhewoifhewiofhoiew
-        // -- Comment: 数字のみであるとアウト
-        // 58326547236532653275693256236598325628356326858923
-        // --- Comment: 記号のみであるとアウト
-        // ********************************@@@@@@@@@@@
-        // --- Comment: 一つでも必須文字がなければアウト(大文字が含まれていない)
-        // fefhjewhfoewfheoheowhewo6487325837258732@@@
-        // --- Comment: 一つでも必須文字がなければアウト(小文字が含まれていない)
-        // FJEHFOEWHFOEWHFGOEWHGOEWHGOEWH5732563275***@@
-        // --- Comment: 一つでも必須文字がなければアウト(数字が含まれていない)
-        // wqheowqhiqwhoiwqhriowqHDjoiq@@@
-        // --- Comment: 一つでも必須文字がなければアウト(記号が含まれていない)
-        // fnakneoafebiabeiFYUFUFUFU343264823764238768237
-        // --- Comment: password(大文字小文字不問)が含まれるとアウト
-        // 1@48k.TBCpassword
-        // --- Comment: paSSwoRd(大文字小文字不問)が含まれるとアウト
-        // 1@48k.TBCpaSSwoRd
-        // --- Comment: p@ssw0rd(大文字小文字不問)が含まれるとアウト
-        // xj.ArG*GKzT@bkq?1!p@ssw0rDll+7q/ rIBgB+s
-        // --- Comment: p@55w0rd(大文字小文字不問)が含まれるとアウト
-        // jsTUGUwagPp0r?vg2yMS2q+fFX
-        // -- Comment: qwerty(大文字小文字不問)が含まれるとアウト
-        // e9-Mbhhtz9CqwertyH3/FekMCz=dwzo5FC
-        // -- Comment: asdfgh(大文字小文字不問)が含まれるとアウト
-        // QbZ.isbA8!asdFghKU=k#a+Jd3+n/K5XqXf5sFx
-        // -- Comment: (大文字小文字不問)が含まれるとアウト
-        // .Cm@nKHtTW1*3bL8e4Sbgs0ooE4+e!Q@=BZdqcfElWsEGcGFm!j-VYAjUiT*X@Y6PGko?.@d_tT5@dNtQ/Q!h*bAKhR..#xqS
-        // -- Comment: zxcvbn(大文字小文字不問)が含まれるとアウト
-        // j#5zA+@*JNh+-7ZXCVBNvfqi_jOBVKmMw?fL*-dbXVk
-        // -- Comment: abcdef(大文字小文字不問)が含まれるとアウト
-        // /8.zeKCg*.myaAbcDefabcdef1Bp0UVwWSuJdp0Gw6lGZZe+3JrlD7@OhdQ9We!
-        // -- Comment: 123456が含まれるとアウト
-        // osPEirT_JEq.!GANHmIi5123456_pJ=bf_CY++a=ScLNZsMz1KVikse2*34nwEVtVgW_xj?nr
-
+//        以下はテストコード
+//        passwordsForTest.stream().forEach(o ->{
+//            try {
+//                if(PasswordManager.validate(o)) System.out.printf("%s -> ok\n",o);
+//            } catch (PasswordRulesViolationException e) {
+//                System.out.printf("%s -> ",o);
+//                System.out.printf("パスワードのルール基準を満たしていません。%s \n", e.getMessage());
+//            }
+//        });
     }
 }
